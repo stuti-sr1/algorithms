78a27b38f520185a1179c5a16dca6e7a39b8519f
static-vector <stutisrivastava90@gmail.com>
2021-04-30 06:49:34 +0800

    adding code

---
+++ Maths/EasyCollection/942.DIStringMatch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCollection
+{
+    public static class DIStringMatch
+    {
+
+        public static int[] DiStringMatch(string s)
+        {
+            int len = s.Length, i = 0;
+            int j = len, k = 0;
+            int[] result = new int[(len + 1)];
+            for (k = 0; k < len; k++)
+            {
+                if (s[k] == 'D') result[k] = j--;
+                else if (s[k] == 'I') result[k] = i++;
+                else return result;
+            }
+            result[k] = i;
+            return result;
+        }
+
+        //public static int[] DiStringMatch(string s)
+        //{
+        //    int j = s.Length, i = 0, k = 0;
+        //    int[] result = new int[(j + 1)];
+        //    foreach (char ch in s)
+        //    {
+        //        if (ch == 'D') result[k++] = j--;
+        //        else if (ch == 'I') result[k++] = i++;
+        //        else return result;
+        //    }
+        //    result[k++] = i;
+        //    return result;
+        //}
+    }
+}
--- Maths/EasyCollection/Program.cs
+++ Maths/EasyCollection/Program.cs
@@ -6,18 +6,21 @@ namespace EasyCollection
     {
         static void Main(string[] args)
         {
-            int nums = 4421;
-            Console.WriteLine(SubtractProductAndSumOfDigitsOfAnInteger.SubtractProductAndSum(nums));
+            //int nums = 4421;
+            //Console.WriteLine(SubtractProductAndSumOfDigitsOfAnInteger.SubtractProductAndSum(nums));
 
-            nums = 9669;
-            Console.WriteLine(Maximum69Number.Maximum69NumberFun(nums));
+            //nums = 9669;
+            //Console.WriteLine(Maximum69Number.Maximum69NumberFun(nums));
 
-            int[] num = { 1, 2, 3, 1, 1, 3 };
-            Console.WriteLine(NumberofGoodPairs.NumIdenticalPairs(num));
+            //int[] num = { 1, 2, 3, 1, 1, 3 };
+            //Console.WriteLine(NumberofGoodPairs.NumIdenticalPairs(num));
 
-            num =new int[] { -1, 1, -1, 1, -1 };
-            Console.WriteLine(SignOfTheProductOfAnArray.ArraySign(num));
+            //num =new int[] { -1, 1, -1, 1, -1 };
+            //Console.WriteLine(SignOfTheProductOfAnArray.ArraySign(num));
 
+            //Console.WriteLine(SumOfDigitsInBaseK.SumBase(34,6));
+
+            Console.WriteLine(DIStringMatch.DiStringMatch("IDID"));
             Console.ReadKey();
         }
     }
