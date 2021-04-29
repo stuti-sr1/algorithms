cd3f613952692ed1dc94052f2f0b5197f986de25
static-vector <stutisrivastava90@gmail.com>
2021-04-30 06:48:08 +0800

    leet code changes for Binary search
    leet code changes

---
+++ BinarySearch/BinarySearch/1337.TheKWeakestRowsInAMatrix.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public static class TheKWeakestRowsInAMatrix
+    {
+        public static int[] KWeakestRows(int[][] mat, int k)
+        {
+            IDictionary<int, int> map = new Dictionary<int, int>();
+            int i = 0;
+            foreach (var matRow in mat)
+            {
+                map[i++] = BinarySearchFun(matRow);
+            }
+            return map.OrderBy(x => x.Value).Select(y => y.Key).Take(k).ToArray();
+        }
+
+        private static int BinarySearchFun(int[] matRow)
+        {
+            int mid = 0, l = 0, h = matRow.Length-1;
+            int len = h;
+            while (l <= h)
+            {
+                mid = l + (h - l) / 2;
+                if (matRow[mid] == 1 && (mid == len || matRow[mid + 1] == 0))
+                {
+                    return mid + 1;
+                }
+                else if (matRow[mid] == 1) l = mid + 1;
+                else h = mid - 1;
+            }
+            return mid;
+        }
+        //public static int[] KWeakestRows(int[][] mat, int k)
+        //{
+        //    IDictionary<int, int> map = new Dictionary<int, int>();
+        //    int i = 0;
+        //    foreach (var matRow in mat)
+        //    {
+        //        int soldierCount = 0;
+        //        foreach (int col in matRow)
+        //        {
+        //            if (col == 1) soldierCount++;
+        //            else break;
+        //        }
+        //        map[i++] = soldierCount;
+        //    }
+        //    return map.OrderBy(x => x.Value).Select(y => y.Key).Take(k).ToArray();
+        //}
+    }
+}
---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] mat = new int[5][];
+            mat[0] = new int[] { 1, 1, 0, 0, 0 };
+            mat[1] = new int[] { 1, 1, 1, 1, 0 };
+            mat[2] = new int[] { 1, 0, 0, 0, 0 };
+            mat[3] = new int[] { 1, 1, 0, 0, 0 };
+            mat[4] = new int[] { 1, 1, 1, 1, 1 } ;
+            var t = TheKWeakestRowsInAMatrix.KWeakestRows(mat, 3);
+           // Console.WriteLine(TheKWeakestRowsInAMatrix.KWeakestRows(mat,3));
+            Console.ReadKey();
+        }
+    }
+}
