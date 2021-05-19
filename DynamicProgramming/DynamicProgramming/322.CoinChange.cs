e8faa376ef55c8ad98ce09a9f2143b860f4a8445
static-vector <stutisrivastava90@gmail.com>
2021-05-20 00:47:34 +0800

    DP code implementation

---
+++ DynamicProgramming/DynamicProgramming/322.CoinChange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    public static class CoinChange
+    {
+        public static int CoinChangeFun(int[] coins, int amount)
+        {
+            int[] dp = new int[amount + 1];
+            for (int i = 0; i < dp.Length; i++)
+            {
+                dp[i] = amount+1;
+            }
+            dp[0] = 0;
+            foreach (int coin in coins)
+            {
+                for (int j = 0; j < dp.Length; j++)
+                {
+                    if (coin <= j)
+                    {
+                        dp[j] = Math.Min(dp[j], 1 + dp[j - coin]);
+                    }
+                }
+            }
+            return dp[amount] != amount + 1 ? dp[amount] : -1;
+        }
+    }
+}
---
+++ DynamicProgramming/DynamicProgramming/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DynamicProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] coins = { 1, 2, 5 };
+            //int amount = 11;
+            int[] coins = { 2};
+            int amount = 3;
+            Console.WriteLine(CoinChange.CoinChangeFun(coins, amount));
+            Console.ReadKey();
+        }
+    }
+}
