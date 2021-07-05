e6e5d718600a18303e6954c10382dc31ea10eb15
static-vector <stutisrivastava90@gmail.com>
2021-07-05 23:47:43 +0800

    medium recursion

---
+++ Recursion/RecursionMedium/1823. Find the Winner of the Circular Game.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionMedium
+{
+    public static class FindtheWinneroftheCircularGame
+    {
+        public static int FindTheWinner(int n, int k)
+        {
+            ListNode list = new ListNode(1);
+            ListNode head = list;
+            for (int i = 2; i <= n; i++)
+            {
+                list.next = new ListNode(i);
+                list = list.next;
+            }
+            list.next = head;
+            ListNode prev = list;
+            return ReturnWinner(head, k, prev);
+        }
+
+        private static int ReturnWinner(ListNode head, int k,ListNode prev)
+        {
+            while (prev != head)
+            {
+                int count = 0;
+                while (count < k-1)
+                {
+                    prev = head;
+                    head = head.next;
+                    count++;
+                }
+                if (head != null && prev != head)
+                {
+                    prev.next = head.next;
+                    head = head.next;
+                }
+            }
+            return head.value;
+        }
+    }
+    public class ListNode
+    {
+        public int value { get; set; }
+        public ListNode next { get; set; }
+        public ListNode(int value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+}
---
+++ Recursion/RecursionMedium/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RecursionMedium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5, k = 2;
+            Console.WriteLine(FindtheWinneroftheCircularGame.FindTheWinner(n,k));
+            Console.WriteLine(FindtheWinneroftheCircularGame.FindTheWinner(6,5));
+            Console.ReadKey();
+        }
+    }
+}
